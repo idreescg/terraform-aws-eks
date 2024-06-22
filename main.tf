fb29f4083a2e5bfdf08b9afada7541f1e6ef3f9b
IDHRES AHMED MI <48350502+idreescg@users.noreply.github.com>
2024-06-22 19:04:00 +0800

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,37 @@
+provider "aws" {
+  region = "us-west-2" # Specify your preferred region
+}
+
+module "eks" {
+  source          = "terraform-aws-modules/eks/aws"
+  cluster_name    = "my-cluster"
+  cluster_version = "1.23"
+  subnets         = module.vpc.private_subnets
+  vpc_id          = module.vpc.vpc_id
+
+  worker_groups = [
+    {
+      instance_type = "m5.large"
+      asg_max_size  = 2
+    }
+  ]
+
+  # Enable Amazon EKS control plane logging
+  cluster_enabled_log_types = ["api", "audit", "authenticator", "controllerManager", "scheduler"]
+
+  # Enable Amazon EKS addons
+  enable_irsa = true
+}
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  name    = "my-vpc"
+  cidr    = "10.0.0.0/16"
+
+  azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  enable_nat_gateway = true
+  single_nat_gateway = true
+}
